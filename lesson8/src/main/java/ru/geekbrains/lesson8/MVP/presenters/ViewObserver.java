2b73aee1bb2ff594909118b8e05af88f2ab6a1f0
Aleksandr <kino_mac@mail.ru>
2023-01-04 15:59:10 +0300

    Файлы с семинара

---
+++ lesson8/src/main/java/ru/geekbrains/lesson8/MVP/presenters/ViewObserver.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson8.MVP.presenters;
+
+import java.util.Date;
+
+public interface ViewObserver {
+
+    void onReservationTable(Date reservationDate, int tableNo, String name);
+
+}
